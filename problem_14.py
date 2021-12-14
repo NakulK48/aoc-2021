6022b87372ccf55bf20b40f6b464ac8e90a789d6
Nakul Khanna <nakul.khanna@gmail.com>
2021-12-14 08:45:07 +0000

    Clean up 14b

--- problem_14.py
+++ problem_14.py
@@ -33,7 +33,6 @@ def get_pair_to_frequency(chain: List[str]):
 
 def part_b():
     chain, rules = get_chain_and_rules()
-    first_char = chain[0]
     last_char = chain[-1]
     pair_to_frequency = get_pair_to_frequency(chain)
     for _ in range(40):
@@ -44,20 +43,17 @@ def part_b():
             new_pair_to_frequency[new_element + pair[1]] += current_freq
         pair_to_frequency = new_pair_to_frequency
     
-    # most chars are double counted
-    char_to_double_frequency = defaultdict(int)
+    # only count the first in the pair to avoid double counting
+    char_to_frequency = defaultdict(int)
     for pair, pair_frequency in pair_to_frequency.items():
-        for pair_char in pair:
-            char_to_double_frequency[pair_char] += pair_frequency
+        char_to_frequency[pair[0]] += pair_frequency
 
-    # the first char and last char are not double counted
-    # as they are only in one pair each
-    char_to_double_frequency[first_char] += 1
-    char_to_double_frequency[last_char] += 1
+    # the last char is never first in the pair
+    char_to_frequency[last_char] += 1
 
-    max_freq = max(char_to_double_frequency.values())
-    min_freq = min(char_to_double_frequency.values())
-    return (max_freq - min_freq) // 2
+    max_freq = max(char_to_frequency.values())
+    min_freq = min(char_to_frequency.values())
+    return (max_freq - min_freq)
 
 print(part_a())
 print(part_b())
